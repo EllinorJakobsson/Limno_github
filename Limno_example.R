d951fd0b48dbd7311754449e4c0a230fb27b8de7
Ellinor <ellinor.jakobsson@ebc.uu.se>
2022-04-27 11:31:32 +0200

    I've changed the no. plants

--- Limno_example.R
+++ Limno_example.R
@@ -5,7 +5,7 @@ library(dplyr)
 Some_limno_people <- c("Jorrit", "Ellinor", "Xavi", "Don", "Sofia", "Liam", "Eva", "Silke")
 
 #And their no. plants in the office (one oulier?)
-No_plants_in_office <- c(0, 3, 0, 0, 2, 2, 4, 12)
+No_plants_in_office <- c(1, 3, 1, 1, 2, 2, 4, 4)
 
 #Merge data into one file
 Data <- cbind(Some_limno_people, No_plants_in_office)
