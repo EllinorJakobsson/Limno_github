ca620032b99a0e350ec4977320571f98a97cfeed
Ellinor <ellinor.jakobsson@ebc.uu.se>
2022-04-27 11:30:53 +0200

    I've changed the no. plants and calculated stats

--- Limno_example.R
+++ Limno_example.R
@@ -14,4 +14,9 @@ Data <- as.data.frame(Data)
 #Make no. plants numeric
 Data$No_plants_in_office <- as.numeric(Data$No_plants_in_office)
 
+#I saved it and pushed it to Github here last time
+####NEW EDITS####
+#Calculate mean 
 
+Plants_at_limno_stats <- Data %>% summarise(across(No_plants_in_office, list(mean,sd,sum)))
+#mean 2.875, SD: 3.97986, sum: 23
